4e51a4c2c9bfc378e899ac1696e2da02bed21205
ramya <ramyavijaya3@gmail.com>
2023-03-07 12:20:08 -0600

    function

--- function-lab-hw.js
+++ function-lab-hw.js
@@ -1,13 +1,18 @@
 function minusOne(num){
-    for(let i=70; i>num; i--){
-        console.log(i)
-    }
-  
+  if(typeof Number(num)){
+    num=num-1
+    return num
+  }
 }
 
-minusOne(-2);
+console.log(minusOne("Infinity"));
 
 function  makeSentence(w1,w2,w3){
   return  console.log(w1 +w2+ w3)
 }
 makeSentence("hi "," iam ", " ramya" );
+
+function getLastElement(arr){
+    return arr[arr.length-1]
+}
+console.log(getLastElement([2,3,4]))

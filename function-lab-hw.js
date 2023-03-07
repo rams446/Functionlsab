ddef309adb47376bece6746cfd198f232a02994d
ramya <ramyavijaya3@gmail.com>
2023-03-07 11:53:30 -0600

    Function Lab

---
+++ function-lab-hw.js
@@ -0,0 +1,13 @@
+function minusOne(num){
+    for(let i=70; i>num; i--){
+        console.log(i)
+    }
+  
+}
+
+minusOne(-2);
+
+function  makeSentence(w1,w2,w3){
+  return  console.log(w1 +w2+ w3)
+}
+makeSentence("hi "," iam ", " ramya" );
